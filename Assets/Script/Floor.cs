cd623f6911a81bd6b1c7e17d63e90a703ae90da9
bonochof <ryos427@yahoo.co.jp>
2018-08-30 23:39:05 +0900

    fix floor set process

---
+++ Assets/Script/Floor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+  void Update() {
+    GameObject obj = getClickObject();
+    
+    if (obj != null) {
+      Debug.Log(obj.name);
+    }
+  }
+  
+  public GameObject getClickObject() {
+    GameObject result = null;
+    if(Input.GetMouseButtonDown(0)) {
+　　　   Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+　　　   RaycastHit hit = new RaycastHit();
+　　　   if (Physics.Raycast(ray, out hit)){
+　　　　　   result = hit.collider.gameObject;
+　　　   }
+  　 }
+　   return result;
+  }
+}
